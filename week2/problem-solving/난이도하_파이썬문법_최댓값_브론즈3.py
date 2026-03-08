71e66ad2181a300bc5d0f5594d52c023f0c28e31
윤형민 <hyeongmin92@gmail.com>
2026-03-08 21:28:15 +0900

    solve: 난이도하_파이썬문법_최댓값_브론즈3

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,50 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+
+def get_number_input():
+    nums = []
+    for i in range(9):
+        input_number = int(input())
+        nums.append(input_number)
+    return nums
+
+def print_max_num(nums):
+    
+    max_num = nums[0]
+
+    for num in nums:
+        if num > max_num:
+            max_num = num
+    
+    location = nums.index(max_num)+1
+
+    print(max_num)
+    print(location)
+
+def main():
+    nums = get_number_input()
+    print_max_num(nums)
+
+if __name__ == "__main__":
+    main()
+
+
+## gpt 코칭 후 개선한 코드
+''' 
+
+nums = [int(input()) for _ in range(9)]
+
+max_num = max(nums)
+location = nums.index(max_num) + 1
+
+print(max_num)
+print(location)
+
+'''
+    
+    
+    
+
+    
+
