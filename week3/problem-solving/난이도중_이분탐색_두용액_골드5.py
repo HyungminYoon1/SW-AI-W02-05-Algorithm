64c19573e537631c177d01f68d7791641fe2da80
윤형민 <hyeongmin92@gmail.com>
2026-03-15 21:30:38 +0900

    solve: 난이도중_이분탐색_두용액_골드5

--- week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
+++ week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
@@ -1,2 +1,25 @@
 # 이분탐색 - 두 용액 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2470
+
+n = int(input())
+solutions = sorted(list(map(int, input().split())))
+left_index = 0
+right_index = n-1
+best_sum = float('inf')
+answer = (solutions[left_index], solutions[right_index])
+
+while left_index < right_index:
+    current_sum = solutions[left_index] + solutions[right_index]
+    
+    if abs(current_sum) < abs(best_sum):
+        best_sum = current_sum
+        answer = (solutions[left_index], solutions[right_index])
+
+    if current_sum > 0:
+        right_index -=1
+    elif current_sum < 0:
+        left_index += 1
+    else:
+        break
+
+print(answer[0], answer[1])
