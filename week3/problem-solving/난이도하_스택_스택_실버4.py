ce782faee82e021f966c5da5fdf339a7dde1564a
윤형민 <hyeongmin92@gmail.com>
2026-03-15 13:45:34 +0900

    solve: 난이도하_스택_스택_실버4

--- week3/problem-solving/난이도하_스택_스택_실버4.py
+++ week3/problem-solving/난이도하_스택_스택_실버4.py
@@ -1,2 +1,52 @@
 # 스택 - 스택 (백준 실버 4)
 # 문제 링크: https://www.acmicpc.net/problem/10828
+
+def make_order(stack = [], text = ""):
+
+    if text[:4] == "push":
+        stack.append(int(text.split()[1]))
+        return
+
+    elif text == "pop":
+        return -1 if not stack else stack.pop()
+        
+    elif text == "size":
+        return len(stack)
+    
+    elif text == "empty":
+        return 0 if stack else 1
+            
+    elif text == "top":
+        return stack[-1] if stack else -1
+
+    return
+
+order_count = int(input())
+stack = []
+stacked_orders = []
+for i in range(order_count):
+    stacked_orders.append(make_order(stack, input()))
+for i in range(order_count):
+    if stacked_orders[i] is not None:
+        print(stacked_orders[i])
+        
+
+## gpt가 제시한 최적 코드
+import sys
+
+stack = []
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    cmd = sys.stdin.readline().strip()
+
+    if cmd[:4] == "push":
+        stack.append(int(cmd.split()[1]))
+    elif cmd == "pop":
+        print(stack.pop() if stack else -1)
+    elif cmd == "size":
+        print(len(stack))
+    elif cmd == "empty":
+        print(0 if stack else 1)
+    elif cmd == "top":
+        print(stack[-1] if stack else -1)
