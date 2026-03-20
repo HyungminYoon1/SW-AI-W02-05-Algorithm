2593e6dab042ca0e5bbb8f2b5fc046477da95340
윤형민 <hyeongmin92@gmail.com>
2026-03-21 00:05:13 +0900

    solve: 난이도하_트리_상근이의여행_실버4

--- week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
+++ week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
@@ -1,2 +1,17 @@
 # 트리 - 상근이의 여행 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/9372
+
+# 총 테스트케이스의 수
+test_case = int(input())
+
+for _ in range(test_case):
+    n, m = map(int, input().split()) # 국가의 수 N(2 ≤ N ≤ 1 000)과 비행기의 종류 M(1 ≤ M ≤ 10 000) 
+
+    min_flight_count = n-1 # 모든 정점을 방문 가능하게 만드는 최소 간선 수는 항상 n - 1
+
+    for _ in range(m):
+        t = input()
+    
+    print(min_flight_count)
+        
+
