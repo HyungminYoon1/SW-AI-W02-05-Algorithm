51d4225e9093f3f0bede35a85916bb4ce5099d95
윤형민 <hyeongmin92@gmail.com>
2026-03-27 17:16:06 +0900

    solve: 난이도하_그리디_동전0_실버4

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,21 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+
+n, k = map(int, input().split()) # n: 동전 종류, k: 가치합
+
+# 동전 종류 리스트 초기화
+money_units = []
+for i in range(n):
+    money_units.append(int(input()))
+
+total_count = 0
+
+for money_unit in reversed(money_units):
+    unit_count = k // money_unit
+    k = k % money_unit
+    total_count += unit_count
+
+if k != 0:
+    raise ValueError("값이 동전으로 나누어 떨어지지 않습니다.")
+else:
+    print(total_count)
