d580f2a7ec9b0b06c97e108462113a3407724eeb
윤형민 <hyeongmin92@gmail.com>
2026-03-27 17:42:08 +0900

    solve: 난이도하_DP_피보나치수2_브론즈1

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,18 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+
+n = int(input())
+
+def fib(n):
+
+    if n == 0 or n == 1:
+        return n
+
+    a, b = (0, 1)
+
+    for i in range(3, n+1):
+        a, b = b, a + b
+
+    return a + b
+
+print(fib(n))
